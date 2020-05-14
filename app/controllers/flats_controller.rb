240aedfc6ec211f0358f82489ef6be322f460d5a
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-14 09:45:57 +0200

    search functionality added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,13 @@
 class FlatsController < ApplicationController
+
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
+    @flats = Flat.where("name LIKE '#{params['query']}%'")
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -16,9 +19,26 @@ class FlatsController < ApplicationController
     @flat.save ? (redirect_to flats_path) : (render :new)
   end
 
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
   def flat_params
-    params.require(:flat).permit(:name, :address, :number_of_guests, :price_per_night)
+    params.require(:flat).permit(:name, :address, :picture_url, :number_of_guests, :price_per_night)
   end
 end
